08fc3d8b1b6b9587a17b64f3a9ffdd48f30571b8
stevezmac <stevezmacdonald@gmail.com>
2019-05-20 10:27:41 -0400

    back to normal sh file

--- docker_push.sh
+++ docker_push.sh
@@ -28,9 +28,9 @@ aws configure set default.region ${EB_REGION}
 $(aws ecr get-login --no-include-email)
 
 # update latest version
-docker tag stevezmac/ecr-tutorial:1:0 ${TARGET_IMAGE_LATEST}  #${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
+docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
 docker push ${TARGET_IMAGE_LATEST}
 
 # push new version
-#docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_VERSIONED}
-#docker push ${TARGET_IMAGE_VERSIONED}
+docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_VERSIONED}
+docker push ${TARGET_IMAGE_VERSIONED}
